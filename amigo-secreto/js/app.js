a453f0950f07447a5e5dc8a09953ae425a2c3be2
André Sena <andre-sena@outlook.com.br>
2025-05-07 22:27:50 -0300

    Desafio 5

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,57 @@
+//Incluir novo amigo
+let primeiroCadastro = true;
+function adicionar() {
+    let novoNome = document.getElementById("nome-amigo").value;
+    
+    if (primeiroCadastro == true) {
+        document.getElementById("lista-amigos").textContent += novoNome;
+        primeiroCadastro = false;
+    } else {
+        document.getElementById("lista-amigos").textContent += `,${novoNome}`;
+    }
+
+}
+//Sortear novo amigo
+function sortear() {
+    nomesSorteio = document.getElementById("lista-amigos").textContent.split(",");
+    let presenteadoresSorteados = [];
+    let presenteadosSorteados = [];
+    let textoNomesSorteados = "";
+    let numeroAleatorioPresenteador;
+    let numeroAleatorioPresenteado;
+
+    if (nomesSorteio.length < 2) {
+        alert("Precisa de pelo menos 2 nomes para realizar sorteio!");
+        return;
+    }
+
+    for (let i = 0; i < nomesSorteio.length; i++) {
+        //Pessoa que vai dar o presente
+        do {
+            numeroAleatorioPresenteador = parseInt(Math.random() * nomesSorteio.length);
+        } while (presenteadoresSorteados.includes(numeroAleatorioPresenteador));
+        presenteadoresSorteados.push(numeroAleatorioPresenteador);
+
+        //Pessoa que vai receber o presente
+        do {
+            numeroAleatorioPresenteado = parseInt(Math.random() * nomesSorteio.length);
+            if (numeroAleatorioPresenteado == numeroAleatorioPresenteador && nomesSorteio.length == i + 1) {
+                sortear();
+                return;
+            }
+        } while (presenteadosSorteados.includes(numeroAleatorioPresenteado) || numeroAleatorioPresenteado == numeroAleatorioPresenteador);
+
+        presenteadosSorteados.push(numeroAleatorioPresenteado);
+
+        //Formata texto final
+        textoNomesSorteados += `${nomesSorteio[numeroAleatorioPresenteador]} -> ${nomesSorteio[numeroAleatorioPresenteado]}<br>`;
+
+    }
+    document.getElementById("lista-sorteio").innerHTML = textoNomesSorteados;
+}
+
+function reiniciar() {
+    document.getElementById("lista-amigos").textContent = "";
+    document.getElementById("lista-sorteio").innerHTML = "";
+    primeiroCadastro = true;
+}
